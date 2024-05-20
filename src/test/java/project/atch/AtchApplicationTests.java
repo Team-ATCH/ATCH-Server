e986bf36cf70d04d2713dbdf04eaf09a75c6571d
‘yj-leez’ <yj_leez@naver.com>
2024-05-20 11:01:36 +0900

    init

---
+++ src/test/java/project/atch/AtchApplicationTests.java
@@ -0,0 +1,13 @@
+package project.atch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
