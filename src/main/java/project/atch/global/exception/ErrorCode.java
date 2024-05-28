5900ca8f1a9df95204383f1f04c50ec0cf0be3b9
‘yj-leez’ <yj_leez@naver.com>
2024-05-28 16:08:32 +0900

    feat: 공통 에러 처리 #3

---
+++ src/main/java/project/atch/global/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package project.atch.global.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorCode {
+
+    /* 400 BAD REQUEST */
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    USER_NOT_IN_CHAT_ROOM(HttpStatus.BAD_REQUEST,"잘못된 방에 요청하였습니다."),
+    USER_INFORMATION_NOT_FOUND(HttpStatus.BAD_REQUEST,"상대방의 정보를 찾을 수 없습니다."),
+
+    /* 401 Unauthorized */
+    USER_NOT_FOUND_IN_SESSION(HttpStatus.UNAUTHORIZED, "웹소켓 통신 중 세션에서 사용자를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+}
