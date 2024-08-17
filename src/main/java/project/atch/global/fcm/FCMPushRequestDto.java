5a279b57207de1a64dd6c64289af6b41fef23ff7
‘yj-leez’ <yj_leez@naver.com>
2024-08-17 15:45:14 +0900

    feat: fcm 도입 #15

---
+++ src/main/java/project/atch/global/fcm/FCMPushRequestDto.java
@@ -0,0 +1,24 @@
+package project.atch.global.fcm;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FCMPushRequestDto {
+
+    private String targetToken;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String title;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String body;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String image;
+}
