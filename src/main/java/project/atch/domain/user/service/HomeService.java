649e7798a8c8ece853104b011c093539ebda7e7a
‘yj-leez’ <yj_leez@naver.com>
2024-08-19 15:01:29 +0900

    chore: 라이브러리 정리 #17

--- src/main/java/project/atch/domain/user/service/HomeService.java
+++ src/main/java/project/atch/domain/user/service/HomeService.java
@@ -4,18 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import project.atch.domain.user.dto.ItemDetail;
 import project.atch.domain.user.dto.UserDetailDto;
-import project.atch.domain.user.entity.Item;
 import project.atch.domain.user.entity.User;
-import project.atch.domain.user.repository.ItemRepository;
 import project.atch.domain.user.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
